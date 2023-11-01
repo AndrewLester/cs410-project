1fb1ab1538bb33817e4eeca6772aad3d2f29a46a
AndrewLester <alester220@gmail.com>
2023-11-01 16:06:17 -0500

    Add search

--- README.md
+++ README.md
@@ -8,6 +8,18 @@ Install Anaconda.
 conda create -n cs410
 conda activate cs410
 conda config --env --set subdir osx-64
-conda install python=3.5
+conda install python=3.6.13
 pip install metapy ir_datasets
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+python load_data.py
+```
+
+```bash
+python search_eval.py
 ```
---
+++ search_eval.py
@@ -0,0 +1,44 @@
+import math
+import sys
+import time
+import metapy
+import pytoml
+
+
+config_file = 'config.toml'
+top_results = 10
+
+if __name__ == '__main__':
+    index = metapy.index.make_inverted_index(config_file)
+    ranker = metapy.index.OkapiBM25(k1=1.5,b=0.75,k3=3.5)
+    # eval = metapy.index.IREval(config_file)
+
+    with open(config_file, 'r') as f:
+        config = pytoml.load(f)
+
+    query_cfg = config['query-runner']
+
+    start = time.time()
+    query_path = query_cfg.get('query-path', 'queries.txt')
+    query_start = query_cfg.get('query-id-start', 0)
+
+    query = metapy.index.Document()
+    ndcg = 0.0
+    num_queries = 0
+    print('Running queries')
+    with open(query_path) as f:
+        for query_num, line in enumerate(f):
+            query.content(line.strip())
+            results = ranker.score(index, query, top_results)
+            # ndcg += eval.ndcg(results, query_start + query_num, top_results)
+            num_queries+=1
+            print(f'{query.content()}:', index.metadata(results[0][0]).get('content')[:100])
+            print()
+            if num_queries > 20:
+                break
+
+    # ndcg /= num_queries
+    ndcg = 0
+
+    print(f"NDCG@{top_results}: {ndcg}")
+    print(f"Time: {time.time() - start}")
