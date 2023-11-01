f61b286440818892f31b61bbf00b79dfaf452494
AndrewLester <alester220@gmail.com>
2023-11-01 15:27:38 -0500

    Done

--- load_data.py
+++ load_data.py
@@ -4,3 +4,7 @@ dataset = ir_datasets.load("wikir/en1k/training")
 with open('queries.txt', 'w+') as f:
 	for query in dataset.queries_iter():
 		f.write(query.text + '\n')
+
+with open('wikipedia/wikipedia.dat', 'w+') as f:
+	for doc in dataset.docs_iter():
+		f.write(doc.text + '\n')
